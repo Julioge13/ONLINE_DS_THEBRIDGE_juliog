4ffb45775e231464df145a14b411044b3721ee8b
Julioge13 <julio.ge13@gmail.com>
2023-11-20 10:59:23 +0100

    Create hlf.py

---
+++ hlf.py
@@ -0,0 +1,21 @@
+titulo = [
+    "██░░░░░██ ░░██░░░░░██",
+    "██ ░░░░██ ░░██░░░░░██",
+    "█████████ ░░██░░░░░██",
+    "██ ░░░░██ ░░██░░░░░██",
+    "██ ░░░░██ ░░█████████"
+]
+
+for linea in titulo:
+    print(linea)
+
+titulo_retro = [
+    "H   H  U   U  N   N  DDDD   III  RRRR        L       A         FFFFF   L      OOO  TTTTT     A",
+    "H   H  U   U  NN  N  D   D   I   R   R       L      A A        F       L     O   O   T      A A",
+    "HHHHH  U   U  N N N  D   D   I   RRRR        L     A   A       FFFF    L     O   O   T     A   A",
+    "H   H  U   U  N  NN  D   D   I   R R         L     AAAAA       F       L     O   O   T     AAAAA",
+    "H   H   UUU   N   N  DDDD   III  R  RR       LLLL A     A      F       LLLL   OOO    T    A     A"
+]
+
+for linea in titulo_retro:
+    print(linea)
